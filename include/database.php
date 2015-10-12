cf2237d7a4192bf11bde02cd96ec04de1df94f3e
MKooga <martinkooga@gmail.com>
2015-10-12 12:50:48 +0300

    Contact leht [Delivers #104979684]

--- include/database.php
+++ include/database.php
@@ -1,5 +1,5 @@
 <?php
-require "config.sample.php";
+require "config.php";
 $db = mysqli_connect('DATABASE_HOSTNAME','','DATABASE_DATABASE') or die(mysqli_error($db));
 
 mysqli_query($db, "SET NAMES 'utf8");
